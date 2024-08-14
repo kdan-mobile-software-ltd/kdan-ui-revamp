22dc8f2b0b10512eac7b7bbd7ea2d830e15af6aa
jjustin-chen <justin.chen@kdanmobile.com>
2024-08-14 18:30:45 +0800

    fix: content styles

--- sections/Content/Content41/styled.ts
+++ sections/Content/Content41/styled.ts
@@ -7,6 +7,10 @@ export const GroupWrapper = styled(RwdWrapper)<LayoutNumbersOfColumn>`
   display: grid;
   grid-template-columns: repeat(${({ numbersOfColumn }) => numbersOfColumn || 3}, 1fr);
 
+  @media screen and (${MAX_WIDTH_QUERY.tablet}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
   @media screen and (${MAX_WIDTH_QUERY.mobile}) {
     grid-template-columns: 1fr;
   }
