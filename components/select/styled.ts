45625eb259268b1a4dd3dbd0f54b61cc011c420e
“dena.chen” <dena.chen@kdanmobile.com>
2024-12-31 12:23:24 +0800

    feat: build Select (multi select) and update story

--- components/select/styled.ts
+++ components/select/styled.ts
@@ -1,8 +1,6 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import {
-  BorderColorPropsType,
-  CustomCssPropsType,
-  IsActivePropsType, IsOpenPropsType, ThemePropsType,
+  BorderColorPropsType, CustomCssPropsType, IsOpenPropsType, ThemePropsType,
 } from '@/constants/types/styled';
 import { body5FontStyle } from '@/constants/fonts';
 import { inputBorderStyle } from '../TextField/styled';
@@ -16,31 +14,41 @@ export const InputWrapper = styled.div<ThemePropsType & BorderColorPropsType>`
   position: relative;
   display: flex;
   align-items: center;
-  padding: 0 12px;
+  padding-right: 12px;
   z-index: 20;
   background-color: ${({ theme: { colors } }) => colors.white};
   width: 100%;
   cursor: pointer;
 `;
 
-export const Dropdown = styled.ul<IsOpenPropsType & DropdownCustomStyle>`
+export const Dropdown = styled.ul<IsOpenPropsType & DropdownCustomStyle & CustomCssPropsType>`
   box-shadow: 0px 0px 12px 0px ${({ theme }) => theme.colors.boxShadow};
   position: absolute;
   border-radius: 4px;
   overflow: scroll;
-  z-index: 109 !important;
+  z-index: 100;
   width: 100%;
   max-height: ${({ maxHeight }) => maxHeight || '220px'};
-  transition: opacity ease-in-out .1s;;
+  transition: all ease-in-out .1s;;
 
-  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
+  ${({ isOpen }) => (isOpen
+    ? css`
+      opacity: 1;
+      visibility: unset;
+    ` : css`
+      opacity: 0;
+      visibility: hidden;
+    `
+  )}
 
   left: 0;
   top: ${({ openDirection }) => (openDirection === 'downward' ? '100%' : 'auto')};
   bottom: ${({ openDirection }) => (openDirection === 'upward' ? '100%' : 'auto')};
+  ${({ customCss }) => customCss}
 `;
 
-export const Option = styled.option<ThemePropsType & IsActivePropsType & CustomCssPropsType>`
+export type OptionStyleType = ThemePropsType & CustomCssPropsType & { isBottom?: boolean; };
+export const Option = styled.option<OptionStyleType>`
   padding: 10px 12px;
   transition: background-color ease-in-out .1s;
   background-color: ${({ theme }) => theme.colors.white};
